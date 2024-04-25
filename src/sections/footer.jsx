1454a52f601ad019b5934eaf229308c3c4c8c4a8
muhammetsenses <70330826+muhammetsenses@users.noreply.github.com>
2024-04-25 18:26:35 +0300

    footer.jsx

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -2,7 +2,7 @@
 
 function Footer(){
     return(
-        <footer className="py-5 bg-dark">
+        <footer className="py-4 bg-dark">
             <p className="m-0 text-center text-white">Copyright © Muhammet Senses 2024</p>
         </footer>
     )
